a8d9707a5075ebd774682d559af0da811bc35236
Falydoor <mib.ftw@gmail.com>
2013-05-06 01:55:05 +0200

    Format and remove optionnal libs

--- js/home.js
+++ js/home.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $('#station-trades-header').click(function () {
-        $('#station-trades-table').toggle();
-    });
+	$('#station-trades-header').click(function() {
+		$('#station-trades-table').toggle();
+	});
 
-    $('#penury-trades-header').click(function () {
-        $('#penury-trades-table').toggle();
-    });
+	$('#penury-trades-header').click(function() {
+		$('#penury-trades-table').toggle();
+	});
 
-    $('#hub-trades-header').click(function () {
-        $('#hub-trades-table').toggle();
-    });
+	$('#hub-trades-header').click(function() {
+		$('#hub-trades-table').toggle();
+	});
 
 });
